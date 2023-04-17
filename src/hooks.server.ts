87661ee335620f477081e854eb01831a0ad7c55d
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-04-17 11:16:00 +0700

    cesar/sc-97_run-bulk-reformat-wrt-prettier-config (#9)
    * 🎨 chore: bulk reformat wrt prettier config
    
    * 🙈 chore: add bulk change commit to .git-blame-ignore-revs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,28 +1,28 @@
-import type { HandleServerError } from '@sveltejs/kit';
-import crypto from 'crypto';
-import * as Sentry from '@sentry/node';
-import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+import type { HandleServerError } from '@sveltejs/kit'
+import crypto from 'crypto'
+import * as Sentry from '@sentry/node'
+import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 
 // Only emit errors in production
 if (import.meta.env.PROD) {
-	Sentry.init({
-		dsn: PUBLIC_SENTRY_DSN,
-		tracesSampleRate: 1.0
-	});
+  Sentry.init({
+    dsn: PUBLIC_SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  })
 }
 
 export const handleError: HandleServerError = ({ error, event }) => {
-	const errorId = crypto.randomUUID();
+  const errorId = crypto.randomUUID()
 
-	// Only emit errors in production
-	if (import.meta.env.PROD) {
-		Sentry.captureException(error, {
-			contexts: { sveltekit: { event, errorId } }
-		});
-	}
+  // Only emit errors in production
+  if (import.meta.env.PROD) {
+    Sentry.captureException(error, {
+      contexts: { sveltekit: { event, errorId } },
+    })
+  }
 
-	return {
-		message: 'An error occurred. I have spoken.',
-		errorId
-	};
-};
+  return {
+    message: 'An error occurred. I have spoken.',
+    errorId,
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <h1 class="text-8xl text-cyan-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button
-	class="btn-primary btn"
-	on:click={() => {
-		throw new Error('sentry works')
-	}}>DaisyUI</button
+  class="btn-primary btn"
+  on:click={() => {
+    throw new Error('sentry works')
+  }}>DaisyUI</button
 >
