ccd0bde49f5aa04517fd43095ee9d8011e93c207
Cesar Mejia <cmejia@gmail.com>
2023-04-17 10:49:15 +0700

    🧑‍💻 build: customize prettier config and add prettier tailwind plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<h1 class="text-8xl">Welcome to SvelteKit</h1>
+<h1 class="text-8xl text-cyan-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button
-	class="btn btn-primary"
+	class="btn-primary btn"
 	on:click={() => {
-		throw new Error('sentry works');
+		throw new Error('sentry works')
 	}}>DaisyUI</button
 >
