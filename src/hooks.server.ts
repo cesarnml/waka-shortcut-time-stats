fe5656c15f52e127da06f094826d4dcbc826c355
Cesar Mejia <cmejia@gmail.com>
2023-04-17 16:34:01 +0700

    🛂 feat: add supabase, magic link auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,47 @@
-import type { HandleServerError } from '@sveltejs/kit'
+import type { Handle, HandleServerError } from '@sveltejs/kit'
 import crypto from 'crypto'
 import * as Sentry from '@sentry/node'
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
+import {
+  PUBLIC_SENTRY_DSN,
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+} from '$env/static/public'
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 
 // Only emit errors in production
 if (import.meta.env.PROD) {
   Sentry.init({
     dsn: PUBLIC_SENTRY_DSN,
     tracesSampleRate: 1.0,
+  })
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event,
+  })
+
+  /**
+   * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
+   */
+  event.locals.getSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession()
+    return session
+  }
+
+  return resolve(event, {
+    /**
+     * There´s an issue with `filterSerializedResponseHeaders` not working when using `sequence`
+     *
+     * https://github.com/sveltejs/kit/issues/8061
+     */
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range'
+    },
   })
 }
 
