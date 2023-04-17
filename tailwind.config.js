de1ff844874e76243cd48cf0201cc0da8bed3759
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-04-17 09:39:30 +0700

    📦 build: add daisyui and fix tailwind plugin config (#5)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ export default {
 	theme: {
 		extend: {}
 	},
+	corePlugins: {
+		aspectRatio: false
+	},
 	plugins: [
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/forms'),
-		require('@tailwindcss/aspect-ratio')
+		require('@tailwindcss/aspect-ratio'),
+		require('daisyui')
 	]
 };
