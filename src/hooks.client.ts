dcbf5fed492b9a77159d4af3fb6571a985d4fec5
Cesar Mejia <cmejia@gmail.com>
2023-04-17 10:03:33 +0700

    📦 build: add sentry error tracking in production

---
+++ src/hooks.client.ts
@@ -0,0 +1,37 @@
+import type { HandleClientError } from '@sveltejs/kit';
+import * as Sentry from '@sentry/svelte';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+
+if (import.meta.env.PROD) {
+	Sentry.init({
+		dsn: PUBLIC_SENTRY_DSN, // can be hardcoded
+		integrations: [
+			new Sentry.BrowserTracing(), // Detail trace stack
+			new Sentry.Replay({
+				// Visual replay of errors (cool!)
+				maskAllInputs: true, // respect user privacy on replays
+				maskAllText: true,
+				blockAllMedia: true
+			})
+		],
+		tracesSampleRate: 1.0, // lower settings if swamped with errors
+		replaysSessionSampleRate: 1.0,
+		replaysOnErrorSampleRate: 1.0
+	});
+}
+
+export const handleError: HandleClientError = ({ error, event }) => {
+	const errorId = crypto.randomUUID(); // add unique errorId for easy reference
+
+	// Only emit errors in production
+	if (import.meta.env.PROD) {
+		Sentry.captureException(error, {
+			contexts: { sveltekit: { event, errorId } }
+		});
+	}
+
+	return {
+		message: 'An error occurred. I have spoken.',
+		errorId
+	};
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+const config = defineConfig(({ mode }) => {
+	// Only setup Sentry in production
+	const isProduction = mode === 'production';
+	// Gain access to environmental variables within vite config
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		build: {
+			sourcemap: true // Tell vite to emit source maps
+		},
+		plugins: [
+			sveltekit(), // Add the sentry-vite-plugin last
+			isProduction
+				? sentryVitePlugin({
+						telemetry: false, // Privacy :-)
+						org: env.PUBLIC_SENTRY_ORG, // can be hardcode
+						project: env.PUBLIC_SENTRY_PROJECT, // can be hardcode
+						authToken: env.SENTRY_AUTH_TOKEN, // must be secret
+						include: './svelte-kit/output' // default build output for source maps. Uploaded to Sentry on deploy
+				  })
+				: ''
+		],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		}
+	};
 });
+
+export default config;
