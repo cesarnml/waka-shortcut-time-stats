e5effecc715941d4de59d3198c1a131e127d02b2
Cesar Mejia <cmejia@gmail.com>
2023-04-17 10:10:03 +0700

    🥅 chore: test that error tracking works in prod

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
 <h1 class="text-8xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn btn-primary">DaisyUI</button>
+<button
+	class="btn btn-primary"
+	on:click={() => {
+		throw new Error('sentry works');
+	}}>DaisyUI</button
+>
