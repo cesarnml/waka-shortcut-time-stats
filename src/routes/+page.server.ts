c9170b3d5d20943dfde555d11380807c99831199
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-04-20 08:10:26 +0700

    fix: :adhesive_bandage: remove unnecessary await

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -9,13 +9,9 @@ export const load = async ({ fetch }) => {
       fetch('/api/wakatime/current/all-time-since-today'),
       fetch('/api/wakatime/current/summaries'),
       fetch('/api/wakatime/current/projects'),
-      await fetch('/api/shortcut/iterations'),
+      fetch('/api/shortcut/iterations'),
     ])
 
-  // const response = await fetch('/api/wakatime/current/all-time-since-today')
-  // const summariesResponse = await fetch('/api/wakatime/current/summaries')
-  // const projectsResponse = await fetch('/api/wakatime/current/projects')
-  // const iterationsResponse = await fetch('/api/shortcut/iterations')
   const allTimeSinceToday = (await allTimeResponse.json()) as AllTimeSinceTodayData
   const summaries = (await summariesResponse.json()) as SummariesResult
   const projects = (await projectsResponse.json()) as WakaProjectResult
