ff021fa2c4ec20199f20ac5e680405f12879e57c
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-04-28 08:28:25 +0700

    feat: ✨ navigate to story permalink from branch vs time chart

---
+++ src/routes/api/shortcut/search/stories/+server.ts
@@ -0,0 +1,20 @@
+import { SHORTCUT_API_TOKEN } from '$env/static/private'
+import type { StorySearchResults } from '$lib/generated/openapi/shortcut'
+import { json } from '@sveltejs/kit'
+
+const BASE_URL = 'https://api.app.shortcut.com/api/v3'
+const RESOURCE = '/search/stories'
+
+export const GET = async ({ fetch, url }) => {
+  const query = url.searchParams.get('query')
+
+  const response = await fetch(`${BASE_URL}${RESOURCE}?query=${query}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Shortcut-Token': SHORTCUT_API_TOKEN,
+    },
+  })
+  const storySearchResults: StorySearchResults = await response.json()
+  return json(storySearchResults)
+}
