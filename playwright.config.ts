d9dba0622c1951b5c2899b5f6e549e5d8300700e
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-02 13:59:38 +0700

    fix(test): ✅ add missing import

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices, type PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   testDir: 'tests',
