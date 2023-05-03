ebb6fb0a4a6445f3bf576c5d535a545594974184
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-03 12:54:33 +0700

    feat: ✨ add dynamic date update to chart

--- src/lib/components/CodingActivityLineChart.svelte
+++ src/lib/components/CodingActivityLineChart.svelte
@@ -4,30 +4,82 @@
   import { onMount, afterUpdate } from 'svelte'
   import { page } from '$app/stores'
   import type { SummariesResult } from '$src/routes/api/wakatime/current/summaries/+server'
+  import ChartContainer from './ChartContainer.svelte'
+  import ChartTitle from './ChartTitle.svelte'
+  import { DateFormat, secPerHour } from '$lib/constants'
 
   export let summaries: SummariesResult
+  export let title = 'Coding Activity'
+  export let chartRef: HTMLDivElement
 
-  $: dates = summaries.data.map((summary) => dayjs(summary.range.date).format('MMM DD'))
+  let chart: echarts.ECharts
+  $: dates = summaries.data.map((summary) => dayjs(summary.range.date).format(DateFormat.Short))
   $: values = summaries.data.map((summary) =>
-    (summary.grand_total.total_seconds / 60 / 60).toFixed(1),
+    (summary.grand_total.total_seconds / secPerHour).toFixed(1),
   )
 
-  let myChart: echarts.ECharts
   let option: echarts.EChartsOption
 
+  option = {
+    tooltip: {
+      trigger: 'item',
+      valueFormatter: (value) => `${value}h`,
+    },
+    grid: { left: 50, right: 20, top: 50, bottom: 50, containLabel: true },
+
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: dates,
+        axisLabel: {
+          color: '#fafafa',
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        axisLabel: {
+          showMinLabel: false,
+
+          formatter: (value: string) => `${value}h`,
+          color: '#fafafa',
+        },
+      },
+    ],
+    // @ts-expect-error tough type
+    series: {
+      name: $page.params.projectName,
+      type: 'line',
+      smooth: true,
+      areaStyle: {
+        color: summaries.color ?? '#5A6FC0',
+      },
+      lineStyle: {
+        width: 4,
+        color: summaries.color ?? '#5A6FC0',
+      },
+      itemStyle: {
+        borderCap: 'round',
+        borderJoin: true,
+        color: '#ff0000',
+        borderWidth: 2,
+        borderColor: '#ff0000',
+      },
+      data: values,
+    },
+  }
+
   afterUpdate(() => {
-    if (option && myChart) {
+    if (option && chart) {
       option = {
         tooltip: {
           trigger: 'item',
+          valueFormatter: (value) => `${value}h`,
         },
-        grid: {
-          top: '4%',
-          left: '2%',
-          right: '4%',
-          bottom: '4%',
-          containLabel: true,
-        },
+        grid: { left: 50, right: 20, top: 50, bottom: 50, containLabel: true },
+
         xAxis: [
           {
             type: 'category',
@@ -42,11 +94,14 @@
           {
             type: 'value',
             axisLabel: {
+              showMinLabel: false,
+
               formatter: (value: string) => `${value}h`,
               color: '#fafafa',
             },
           },
         ],
+        // @ts-expect-error tough type
         series: {
           name: $page.params.projectName,
           type: 'line',
@@ -58,77 +113,33 @@
             width: 4,
             color: summaries.color ?? '#5A6FC0',
           },
+          itemStyle: {
+            borderCap: 'round',
+            borderJoin: true,
+            color: '#ff0000',
+            borderWidth: 2,
+            borderColor: '#ff0000',
+          },
           data: values,
         },
       }
 
-      myChart.setOption(option)
+      chart.setOption(option)
     }
   })
+
   onMount(() => {
-    const chartDom = document.getElementById('main')
-    if (chartDom) {
-      myChart = echarts.init(chartDom)
-
-      window.addEventListener(
-        'resize',
-        function () {
-          myChart.resize()
-        },
-        { passive: true },
-      )
-
-      option = {
-        tooltip: {
-          trigger: 'item',
-        },
-        grid: {
-          top: '4%',
-          left: '2%',
-          right: '4%',
-          bottom: '4%',
-          containLabel: true,
-        },
-        xAxis: [
-          {
-            type: 'category',
-            boundaryGap: false,
-            data: dates,
-            axisLabel: {
-              color: '#fafafa',
-            },
-          },
-        ],
-        yAxis: [
-          {
-            type: 'value',
-            axisLabel: {
-              formatter: (value: string) => `${value}h`,
-              color: '#fafafa',
-            },
-          },
-        ],
-        series: {
-          name: $page.params.projectName,
-          type: 'line',
-          smooth: true,
-          areaStyle: {
-            color: summaries.color ?? '#5A6FC0',
-          },
-          lineStyle: {
-            width: 4,
-            color: summaries.color ?? '#5A6FC0',
-          },
-          data: values,
-        },
-      }
-
-      myChart.setOption(option)
+    const handleResize = () => chart.resize()
+    if (chartRef) {
+      chart = echarts.init(chartRef, 'dark', { renderer: 'svg' })
+      window.addEventListener('resize', handleResize, { passive: true })
+      chart.setOption(option)
     }
+    return () => window.removeEventListener('resize', handleResize)
   })
 </script>
 
-<div class="space-y-8 rounded-2xl bg-slate-800 p-4">
-  <h2 class="text-center text-3xl text-stone-300">Coding Activity Line Chart</h2>
-  <div id="main" class="mb-8 h-96 w-full" />
-</div>
+<ChartContainer>
+  <ChartTitle>{title}</ChartTitle>
+  <div class="h-96 w-full" bind:this={chartRef} />
+</ChartContainer>
