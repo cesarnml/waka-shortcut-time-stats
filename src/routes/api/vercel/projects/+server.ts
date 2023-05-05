cf5323045bdf49921338c57108c29e69ac9ea502
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-05 21:37:59 +0700

    bump

--- src/routes/api/vercel/projects/+server.ts
+++ src/routes/api/vercel/projects/+server.ts
@@ -1,5 +1,16 @@
+import { VERCEL_API_TOKEN } from '$env/static/private'
+import { json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
+import type { ProjectsResult } from '$src/types/vercel'
 
 export const GET: RequestHandler = async () => {
-  return new Response()
+  const baseUrl = 'https://api.vercel.com'
+  const resource = '/v9/projects'
+  const headers = {
+    Authorization: `Bearer ${VERCEL_API_TOKEN}`,
+    'Content-Type': 'application/json',
+  }
+  const response = await fetch(`${baseUrl}${resource}`, { headers })
+  const projectResult: ProjectsResult = await response.json()
+  return json(projectResult)
 }
--- src/routes/projects/[projectName]/+page.server.ts
+++ src/routes/projects/[projectName]/+page.server.ts
@@ -1,9 +1,27 @@
+import type { AliasesResult, ProjectsResult } from '$src/types/vercel'
 import type { SummariesResult } from '$src/types/wakatime'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = (async ({ fetch, params }) => {
-  const response = await fetch(`/api/wakatime/current/summaries?project=${params.projectName}`)
-  const summaries: SummariesResult = await response.json()
+export const load: PageServerLoad = async ({ fetch, params }) => {
+  const responses = await Promise.all([
+    fetch(`/api/wakatime/current/summaries?project=${params.projectName}`),
+    fetch(`/api/vercel/projects`),
+  ])
 
-  return { summaries }
-}) satisfies PageServerLoad
+  const [summaries, projects] = (await Promise.all(responses.map((r) => r.json()))) as [
+    SummariesResult,
+    ProjectsResult,
+  ]
+  const projectId = projects.projects.find((project) => project.link.repo === params.projectName)
+    ?.id as string
+  console.log('projectIeeed:', projectId)
+
+  debugger
+  const response = await fetch(`/api/vercel/aliases/${projectId}`)
+  debugger
+  const aliases = (await response.json()) as AliasesResult
+  console.log('nang:', projectId)
+  debugger
+
+  return { summaries, aliases }
+}
