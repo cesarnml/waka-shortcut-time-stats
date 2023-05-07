de073691e7e4f12d3daf98db9b5d56ba7b84095b
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-07 17:09:10 +0700

    improve navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,15 +1,19 @@
-<nav class="navbar rounded-lg px-6 py-3">
-  <div class="navbar-start">
-    <a class="font-bold normal-case text-primary" href="/">CodingStats</a>
-  </div>
-  <div class="navbar-center" />
-  <ul class="navbar-end flex gap-6">
-    <li class="normal-case">
-      <a class="link-hover link-primary link" href="/projects">Projects</a>
-    </li>
-    <li class="normal-case">
-      <a class="link-hover link-primary link" href="/iterations">Iterations</a>
-    </li>
-    <li class="normal-case"><a class="link-hover link-primary link" href="/login">Login</a></li>
-  </ul>
-</nav>
+<div
+  class="sticky top-0 z-10 border-b border-slate-900/10 backdrop-blur-md dark:border-slate-300/10"
+>
+  <nav class="navbar mx-auto max-w-screen-2xl">
+    <div class="navbar-start">
+      <a class="btn-secondary btn font-bold normal-case" href="/">CodingStats</a>
+    </div>
+    <div class="navbar-center" />
+    <ul class="navbar-end flex gap-6">
+      <li class="normal-case">
+        <a class="link-hover link-secondary link" href="/projects">Projects</a>
+      </li>
+      <li class="normal-case">
+        <a class="link-hover link-secondary link" href="/iterations">Iterations</a>
+      </li>
+      <li class="normal-case"><a class="link-hover link-secondary link" href="/login">Login</a></li>
+    </ul>
+  </nav>
+</div>
