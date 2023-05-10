fd0fb6cffa673b4e5a3c8eeb01932a609ddd00b8
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-11 05:41:35 +0700

    minor

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   vitePlugin: {
-    inspector: true,
-    toggleButtonPos: 'bottom-right',
+    experimental: {
+      inspector: true,
+      toggleButtonPos: 'bottom-right',
+    },
   },
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
