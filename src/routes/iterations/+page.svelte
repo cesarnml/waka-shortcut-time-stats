054c3bfef98a9558007ffa25db44c6016c132537
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-10 09:18:36 +0700

    minor

--- src/routes/iterations/+page.svelte
+++ src/routes/iterations/+page.svelte
@@ -36,7 +36,6 @@
       </div>
       <div>
         {#await lazy.iterationStoriesDict[iteration.id] then stories}
-          {console.log('stories:', stories)}
           {#each stories as story}
             <div>{story.name}</div>
           {/each}
