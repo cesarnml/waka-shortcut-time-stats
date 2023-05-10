0a21fca9b8c3d80bcb8fe2cbd06644b8b6964159
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-10 18:33:31 +0700

    minor

--- src/lib/components/FilesTable.svelte
+++ src/lib/components/FilesTable.svelte
@@ -3,6 +3,8 @@
   import { formatTime } from '$lib/helpers/timeHelpers'
   import type { SummariesResult } from '$src/types/wakatime'
   import orderBy from 'lodash/orderBy'
+  import ChartContainer from './ChartContainer.svelte'
+  import ChartTitle from './ChartTitle.svelte'
 
   export let summaries: SummariesResult
 
@@ -49,34 +51,32 @@
     })),
     'time',
     'desc',
-  )
+  ).filter((item) => Boolean(item.time.trim()))
+  $: {
+    console.log('rows:', rows)
+  }
 </script>
 
-<div class="space-y-8 rounded-2xl bg-slate-800 p-4">
-  <h2 class="text-center text-3xl text-stone-300">Files vs Time</h2>
-  <div class="max-h-[500px] overflow-x-auto overflow-y-auto">
-    <table class="table-zebra table w-full">
-      <thead>
-        <tr>
-          <th>Filename</th>
-          <th>Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each rows as row (row.name)}
-          <tr>
-            <td
-              ><a
-                class="link-accent link"
-                href={`https://github.com/cesarnml/waka-shortcut-time-stats/blob/main/${row.name}`}
-                target="_blank"
-                rel="noopener noreferrer">{row.name}</a
-              ></td
-            >
-            <td>{row.time}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+<ChartContainer>
+  <ChartTitle>Files vs Time</ChartTitle>
+  <div class="mb-4 max-h-[500px] overflow-auto px-6">
+    <div class="flex justify-between pb-2 font-bold uppercase">
+      <div>Filename</div>
+      <div>Time</div>
+    </div>
+    <div class="space-y-2">
+      {#each rows as row (row.name)}
+        <div class="flex justify-between">
+          <a
+            class="link-hover link-accent link"
+            href={`https://github.com/cesarnml/waka-shortcut-time-stats/blob/main/${row.name}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            >{row.name}
+          </a>
+          <div>{row.time}</div>
+        </div>
+      {/each}
+    </div>
   </div>
-</div>
+</ChartContainer>
