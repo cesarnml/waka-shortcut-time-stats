77c20d11e198c88ba713b68f559b0e3dd3f43882
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-18 00:07:09 +0700

    remove useless test

--- src/routes/api/wakatime/current/summaries/server.spec.ts
+++ src/routes/api/wakatime/current/summaries/server.spec.ts
@@ -1,16 +1,6 @@
-import { BaseUrl, RestResource } from '$lib/constants'
 import { vi } from 'vitest'
 import type { RequestEvent } from './$types'
 import { GET } from './+server'
-// TODO: Not a useful test, but think about how to test +server.ts files
-describe('getSummaries', () => {
-  it('should return summaries', async () => {
-    const response = await fetch(`${BaseUrl.WakaTime}${RestResource.Summaries}`)
-    expect(response.status).toBe(200)
-    const json = await response.json()
-    expect(json.data.length).toBeGreaterThan(0)
-  })
-})
 
 describe('GET', () => {
   it('it...', async () => {
