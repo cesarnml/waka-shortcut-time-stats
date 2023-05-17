9b5e763e45fe25fdc67a599980ebd5c3e1da988b
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-17 22:30:40 +0700

    ship it

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,18 @@
 import { ApiEndpoint, WakaApiRange, WakaSliceBy } from '$lib/constants'
 import type { DurationsResult, SummariesResult } from '$src/types/wakatime'
+import axios from 'axios'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ fetch, setHeaders, url }) => {
   const range = url.searchParams.get('range') ?? WakaApiRange.Last_7_Days_From_Yesterday
   setHeaders({ 'Cache-Control': 'public, s-maxage=60,  max-age=60' })
-  const [summariesResponse, durationsResponse, durationsByLanguageResponse] = await Promise.all([
+  const [summariesResponse, durationsByLanguageResponse] = await Promise.all([
     fetch(`${ApiEndpoint.SupabaseSummaries}?range=${range}`),
-    fetch(ApiEndpoint.Durations),
     fetch(`${ApiEndpoint.Durations}?slice_by=${WakaSliceBy.Language}`),
   ])
 
+  const { data: durations }: { data: DurationsResult } = await axios.get(ApiEndpoint.Durations)
   const summaries = (await summariesResponse.json()) as SummariesResult
-  const durations = (await durationsResponse.json()) as DurationsResult
   const durationsByLanguage = (await durationsByLanguageResponse.json()) as DurationsResult
 
   return { summaries, durations, durationsByLanguage }
---
+++ src/routes/page.spec.ts
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/svelte'
+import Home from './+page.svelte'
+import { durations, durationsByLanguage, summaries } from '$src/mocks/testData'
+import { WakaApiRange } from '$lib/constants'
+
+describe('Home', () => {
+  it('it...', async () => {
+    render(Home, { data: { summaries, durations, durationsByLanguage } })
+    // Expect a select input with correct default value
+    const selectInput = screen.getByRole('combobox')
+    expect(selectInput).toBeInTheDocument()
+    expect(selectInput).toHaveValue(WakaApiRange.Last_7_Days_From_Yesterday)
+
+    // Expect a StatPanelItem to be visible
+    expect(screen.getByText(/total hours/i)).toBeVisible()
+    // Expect a full panel StatPanelItem to be in the document
+    expect(screen.getByText(/top project/i)).toBeInTheDocument()
+    // Expect the top chart (Activity) to be visible
+    expect(screen.getByText(/activity/i)).toBeVisible()
+  })
+})
