b6639bbf81f7e21ac838536752fd05ea50aed5a4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-19 14:22:49 +0700

    WIP: d32353a improve coverage

--- src/routes/api/wakatime/current/projects/+server.ts
+++ src/routes/api/wakatime/current/projects/+server.ts
@@ -2,14 +2,14 @@ import { WAKA_API_KEY } from '$env/static/private'
 import { json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 import type { WakaProjectResult } from '$src/types/wakatime'
-import { BaseUrl, RestResource, type ToData } from '$lib/constants'
+import { BaseUrl, RestResource, type DataContainer } from '$lib/constants'
 import axios from 'axios'
 
 export const GET: RequestHandler = async ({ url }) => {
   const q = url.searchParams.get('q') ?? ''
   const page = url.searchParams.get('page') ?? 1
 
-  const { data: result }: ToData<WakaProjectResult> = await axios.get(
+  const { data: result }: DataContainer<WakaProjectResult> = await axios.get(
     `${BaseUrl.WakaTime}${RestResource.Projects}?api_key=${WAKA_API_KEY}&page=${page}&q=${q}`,
   )
 
