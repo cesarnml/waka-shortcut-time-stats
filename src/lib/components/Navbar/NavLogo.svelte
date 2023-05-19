758a3594433cae64a2fa7c349914ee34580ef141
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-19 18:31:24 +0700

    logo upgrade

--- src/lib/components/Navbar/NavLogo.svelte
+++ src/lib/components/Navbar/NavLogo.svelte
@@ -5,7 +5,6 @@
 </script>
 
 <button
-  class="btn-link btn p-0 text-lg font-bold normal-case no-underline"
   on:click={() => {
     dropdown.close()
     goto(Url.Home)
@@ -13,9 +12,3 @@
 >
   <slot />
 </button>
-
-<style lang="postcss">
-  .btn:hover {
-    @apply text-secondary no-underline;
-  }
-</style>
