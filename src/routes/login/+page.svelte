d3b5a5f71f0b1586776a2c62feccbb88e9c63fd5
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-20 20:23:01 +0700

    upload

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,18 @@
+<script lang="ts">
+  export let data
+  const { supabase } = data
+  async function signInWithGitHub() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+    console.log('data:', data)
+  }
+</script>
+
 <svelte:head>
-  <title>User Management</title>
+  <title>Login</title>
 </svelte:head>
 
-<div class="row flex-center flex px-4">
-  <div class="col-6 form-widget">Auth</div>
+<div class="grid h-screen place-items-center">
+  <button class="btn-primary btn" on:click={signInWithGitHub}>Sign in with GitHub</button>
 </div>
