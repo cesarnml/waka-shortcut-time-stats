4ba15d1f73e85c37f3286b7724e4a842226c009e
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-20 09:35:46 +0700

    fix: 🐛 fix bug causes by empty summaries data

--- src/lib/components/Stats/statHelpers.ts
+++ src/lib/components/Stats/statHelpers.ts
@@ -1,9 +1,16 @@
-import { createItemNameToTimeDict, getSummaryItems } from '$lib/helpers/chartHelpers'
+import {
+  SummaryItemType,
+  createItemNameToTimeDict,
+  getSummaryItems,
+} from '$lib/helpers/chartHelpers'
 import type { SummariesResult } from '$src/types/wakatime'
+import { isEmpty } from 'lodash'
 import orderBy from 'lodash/orderBy'
 
+export const DEFAULT_EMPTY_MESSAGE = 'N/A'
+
 export const createProjectList = (summaries: SummariesResult) => {
-  const projects = getSummaryItems(summaries, 'projects')
+  const projects = getSummaryItems(summaries, SummaryItemType.Projects)
 
   const projectToTimeDict = createItemNameToTimeDict(projects)
 
@@ -18,6 +25,8 @@ export const createProjectList = (summaries: SummariesResult) => {
 
 export const getTopLanguage = (summaries: SummariesResult) => {
   const languages = summaries.data.flatMap((summary) => summary.languages)
+
+  if (isEmpty(languages)) return DEFAULT_EMPTY_MESSAGE
 
   const languageToTimeDict = languages.reduce((acc, { name, total_seconds }) => {
     acc[name] = (acc[name] ?? 0) + total_seconds
