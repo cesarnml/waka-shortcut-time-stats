69e87d1418e5d0378b1bd56d8a8f7ead331a8fd4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-21 16:28:27 +0700

    brotli!

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { defineConfig, loadEnv } from 'vite'
 import Inspect from 'vite-plugin-inspect'
+import viteCompression from 'vite-plugin-compression'
 
 const config = defineConfig(({ mode }) => {
   // Only setup Sentry in production
@@ -13,19 +14,20 @@ const config = defineConfig(({ mode }) => {
     build: {
       target: 'esnext', // minimize transpilation
       sourcemap: true, // Tell vite to emit source maps
-      chunkSizeWarningLimit: 1000,
-      rollupOptions: {
-        output: {
-          manualChunks: function manualChunks(id) {
-            if (id.includes('echarts')) {
-              return 'echarts'
-            }
-          },
-        },
-      },
+      chunkSizeWarningLimit: 1200,
+      // rollupOptions: {
+      //   output: {
+      //     manualChunks: function manualChunks(id) {
+      //       if (id.includes('echarts')) {
+      //         return 'echarts'
+      //       }
+      //     },
+      //   },
+      // },
     },
     plugins: [
       sveltekit(), // Add the sentry-vite-plugin last
+      viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
       Inspect({
         build: true,
         outputDir: '.vite-inspect',
