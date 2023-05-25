db3aa1c18d8c2112c590b73a09497753701926e9
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-05-25 12:41:41 +0700

    more coverage

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -22,6 +22,9 @@ export const handlers = [
   rest.get(ApiEndpoint.Durations, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(durations))
   }),
+  rest.get(ApiEndpoint.Projects, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(projects))
+  }),
   rest.get(`${BaseUrl.WakaTime}${RestResource.Summaries}`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(summaries))
   }),
