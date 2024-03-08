17600ca5ed8fe19c8925abddc9eaef42899dde43
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-03-08 17:10:11 +0700

    update crons

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,7 @@
   const { supabase } = data
   const signInWithGitHub = async () => {
     const redirect = import.meta.env.PROD
-      ? 'https://codingstats.cesarmejia.com'
+      ? 'https://codingstats.vercel.app'
       : 'http://localhost:5173'
     await supabase.auth.signInWithOAuth({
       provider: 'github',
