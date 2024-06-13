6ecf222c57e359f26a80638f115ad14e49427ab5
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-06-13 23:29:53 +0700

    logs

--- src/routes/api/ip-based-geolocation/+server.ts
+++ src/routes/api/ip-based-geolocation/+server.ts
@@ -4,13 +4,17 @@ import { IP_INFO_TOKEN } from '$env/static/private'
 
 export const GET: RequestHandler = async ({  url, fetch  }) => {
   const userIp = url.searchParams.get('ip')
+  console.log('userIp:', userIp)
   const URL = `https://ipinfo.io/${userIp}?token=${IP_INFO_TOKEN}`;
+  console.log('URL:', URL)
   fetch(URL)
   .then((response) => response.json())
   .then((data) => {
+    console.log('data:', data)
     return json(data)
   })
   .catch((err) => {
+    console.log('err:', err)
     return error(err);
   });
   return json({message: 'something went wrong with ip lookup'})
