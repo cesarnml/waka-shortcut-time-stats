9c357100cd416e7336a5f9a07b865f011fcd55e6
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-06-14 00:20:50 +0700

    again

--- src/routes/api/ip-based-geolocation/+server.ts
+++ src/routes/api/ip-based-geolocation/+server.ts
@@ -1,26 +1,21 @@
-import { json, error } from '@sveltejs/kit'
-import type { RequestHandler } from './$types'
 import { IP_INFO_TOKEN } from '$env/static/private'
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
 
-export const POST: RequestHandler = async ({  url, fetch  }) => {
+export const POST: RequestHandler = async ({ url, fetch }) => {
   const userIp = url.searchParams.get('ip')
   console.log('userIp:', userIp)
-  const URL = `https://ipinfo.io/${userIp}?token=${IP_INFO_TOKEN}`;
+  const URL = `https://ipinfo.io/${userIp}?token=${IP_INFO_TOKEN}`
   console.log('URL:', URL)
-  fetch(URL, {
+
+  const response = await fetch(URL, {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('data:', data)
-    return json(data)
-  })
-  .catch((err) => {
-    console.log('err:', err)
-    return error(err);
-  });
+  const data = response.json()
+  console.log('data:', data)
+  return json(data)
 }
 
 export const OPTIONS: RequestHandler = async () => {
@@ -28,8 +23,8 @@ export const OPTIONS: RequestHandler = async () => {
     headers: {
       'Access-Control-Allow-Origin': '*', // Allow all origins
       'Access-Control-Allow-Methods': 'POST, OPTIONS', // Allow POST and OPTIONS methods
-      'Access-Control-Allow-Headers': 'Content-Type' // Allow only Content-Type header
+      'Access-Control-Allow-Headers': 'Content-Type', // Allow only Content-Type header
     },
-    body: ''
+    body: '',
   })
-};
+}
