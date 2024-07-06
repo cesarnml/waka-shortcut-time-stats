07479c93c9f59b9c09849aca980c2083ff1ed83c
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-07-05 22:46:46 -0700

    interim

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,65 +2,19 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  darkMode: ['class'],
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: ['class'],
   safelist: ['dark'],
   future: {
     hoverOnlyWhenSupported: true,
   },
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
-      colors: {
-        colors: {
-          'chart-dark': '#0F0C28',
-        },
-        aspectRatio: {
-          panoramic: '3/1',
-          chart: '2/1',
-          photo: '3/2',
-          tv: '4/3',
-          portrait: '4/5',
-          tiktok: '9/16',
-        },
-        border: 'hsl(var(--border) / <alpha-value>)',
-        input: 'hsl(var(--input) / <alpha-value>)',
-        ring: 'hsl(var(--ring) / <alpha-value>)',
-        background: 'hsl(var(--background) / <alpha-value>)',
-        foreground: 'hsl(var(--foreground) / <alpha-value>)',
-        primary: {
-          DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
-          foreground: 'hsl(var(--primary-foreground) / <alpha-value>)',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
-          foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
-          foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
-          foreground: 'hsl(var(--muted-foreground) / <alpha-value>)',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
-          foreground: 'hsl(var(--accent-foreground) / <alpha-value>)',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
-          foreground: 'hsl(var(--popover-foreground) / <alpha-value>)',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card) / <alpha-value>)',
-          foreground: 'hsl(var(--card-foreground) / <alpha-value>)',
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          '2xl': '1400px',
         },
       },
       borderRadius: {
@@ -71,9 +25,52 @@ const config = {
       fontFamily: {
         sans: [...fontFamily.sans],
       },
+      colors: {
+        'chart-dark': '#0F0C28',
+      },
+      aspectRatio: {
+        panoramic: '3/1',
+        chart: '2/1',
+        photo: '3/2',
+        tv: '4/3',
+        portrait: '4/5',
+        tiktok: '9/16',
+      },
+      border: 'hsl(var(--border) / <alpha-value>)',
+      input: 'hsl(var(--input) / <alpha-value>)',
+      ring: 'hsl(var(--ring) / <alpha-value>)',
+      background: 'hsl(var(--background) / <alpha-value>)',
+      foreground: 'hsl(var(--foreground) / <alpha-value>)',
+      primary: {
+        DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+        foreground: 'hsl(var(--primary-foreground) / <alpha-value>)',
+      },
+      secondary: {
+        DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
+        foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)',
+      },
+      destructive: {
+        DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+        foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)',
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
+        foreground: 'hsl(var(--muted-foreground) / <alpha-value>)',
+      },
+      accent: {
+        DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
+        foreground: 'hsl(var(--accent-foreground) / <alpha-value>)',
+      },
+      popover: {
+        DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
+        foreground: 'hsl(var(--popover-foreground) / <alpha-value>)',
+      },
+      card: {
+        DEFAULT: 'hsl(var(--card) / <alpha-value>)',
+        foreground: 'hsl(var(--card-foreground) / <alpha-value>)',
+      },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('daisyui')],
   daisyui: {
     styled: true,
     themes: ['synthwave', 'night'],
@@ -84,6 +81,7 @@ const config = {
     prefix: '',
     darkTheme: 'night',
   },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('daisyui')],
 }
 
 export default config
